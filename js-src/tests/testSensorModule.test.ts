e4fe69820cb232ee67311102599eabaf07ffb78c
tket <tanishkai@ketryx.com>
2024-12-19 17:45:45 -0500

    Update testSensorModule.test.ts

--- js-src/tests/testSensorModule.test.ts
+++ js-src/tests/testSensorModule.test.ts
@@ -1,4 +1,4 @@
-describe('Javascript automated test for sensor module @tests:KXITM1JJAMHNV4T8W3BBC3F1GD8RXD2', () => {
+describe('Javascript automated test for sensor module @tests:KXITM2KDZZAART19Q2SDY2HAWGAE3EX', () => {
   it('sensor module functions correctly', () => {
     // This would test the sensor module.
     expect(1 + 2).toBe(3);
