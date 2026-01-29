029da2efd27abd0aa12da9407e127b3e059b63b6
gideonfeifke-prog <gideon.feifke@ketryx.com>
2026-01-29 11:52:33 -0500

    Update createSensorWarning.ts

--- js-src/app/createSensorWarning.ts
+++ js-src/app/createSensorWarning.ts
@@ -4,8 +4,8 @@
  * b) The latest entered sensor reading is outdated according to the defined maximum allowable time interval.
 
  * @itemId:spec-sensor-reading-warning
- * @itemTitle:"Sensor Reading Warning (Javascript)"
- * @itemFulfills:CS-1,KD-13
+ * @itemTitle:"Replace-sensor reminder workflow"
+ * @itemFulfills:APP-28
  * @itemHasParent:spec-sensor-module
  */
 export function createReadingWarning() {
